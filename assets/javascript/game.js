a33c7c6492df23be114741e83660b06a469457cc
Alex Dyagilev <nosskyline@gmail.com>
2017-02-25 01:57:08 -0500

    Changed layout, added comments, added custom CSS

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,61 +1,61 @@
 
-function myguess(id){
-	console.log("id " + id);
-	$("#message").html("");
+function myguess(id){ 
+//this function takes the id of the button pressed and adds that amount to the user total
+//then it compares the user total and the computer number to see if user has won or lost
+	$("#message").html(""); //the wins/losses is erased until the end of the game
 	switch (id)
             {
-               case 'blue': 
+               case 'blue': //if blue button is pressed, total user score is increased by blue amount
                	total += blue;
                break;
             
-               case 'red': 
+               case 'red': //if red button is pressed, total user score is increased by red amount
                	total += red;
                break;
             
-               case 'white': 
+               case 'white': //if white button is pressed, total user score is increased by white amount
                	total += white;
                break;
             
-               case 'yellow': 
+               case 'yellow': //if yellow button is pressed, total user score is increased by yellow amount
                	total += yellow;
                break;
             }
 
-            $("#total").html(total);
-            console.log("Total " + total);
+            $("#total").html(total); //the user's total score is placed into HTML
 
-    if(total == compNum){
+    if(total == compNum){ //if user score is equal to the computer number
     	wins += 1;
     	$("#wins").html(wins);
     	$("#message").html("You win!");
-    	updateRandom();
+    	updateRandom(); //calls the function that updates the computer number and crystal with new values
     }
-    else if (total > compNum) {
+    else if (total > compNum) { //if user score becomes greater than the computer number
     	losses += 1;
     	$("#losses").html(losses);
     	$("#message").html("You lose!");
-    	updateRandom();
+    	updateRandom(); //calls the function that updates the computer number and crystal with new values
     }
 }
 
 function getRandom(min, max) {
+//this function is called to randomize the computer number and the crystal values
+//it is called in the start and within the updateRandom function
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
 function updateRandom(){
-	 compNum = getRandom(19,120);
-	 total = 0;
-     blue = getRandom(1,12);
-     red = getRandom(1,12);
-     white = getRandom(1,12);
-     yellow = getRandom(1,12);
-     $("#total").html(total);
-     $("#compRandom").html(compNum);
-     
-     console.log("updated blue " + blue);
-     console.log("updated red " + red);
-     console.log("updated white " + white);
-     console.log("updated yellow " + yellow);
+//this function is called after a game ends in order to randomize the computer number and crystal values
+//also resets the user total to 0
+	 compNum = getRandom(19,120); //new computer generated number
+	 total = 0; //user total goes back to 0
+   blue = getRandom(1,12); //new value for blue crystal
+   red = getRandom(1,12); //new value for red crystal
+   white = getRandom(1,12); //new value for white crystal
+   yellow = getRandom(1,12); //new value for yellow crystal
+
+   $("#total").html(total); //total of 0 is inserted into HTML
+   $("#compRandom").html(compNum); //new computer number is inserted into HTML
 }
